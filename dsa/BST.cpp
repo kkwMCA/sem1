ec6584755c510266a09c68a7d16f0d23fcb08310
Sumedh ahire <94281812+darkone03@users.noreply.github.com>
2023-12-12 12:11:23 +0530

    Update BST.cpp

--- dsa/BST.cpp
+++ dsa/BST.cpp
@@ -7,12 +7,11 @@ mirror
 */
 #include<stdio.h>
 #include<malloc.h>
-typedef struct node{
-	struct node *left;
-	int data;
-	struct node	*right;
-}node;
 
+typedef struct node{
+	int data;
+	struct node *right,*left;
+}node;
 
 node* create(node* root,int data){
 	if(root==NULL){
@@ -20,17 +19,17 @@ node* create(node* root,int data){
 		root->data=data;
 		root->left=NULL;
 		root->right=NULL;
-		
 		return root;
 	}else{
-		if(root->data > data)
+		if(root->data > data){
 			root->left=create(root->left,data);
-		else
+		}else{
 			root->right=create(root->right,data);
-	}
+		}
+	}	
 }
 
-void inorder(node* root){
+void inorder(node *root){
 	if(root==NULL)
 		return;
 	
@@ -39,85 +38,119 @@ void inorder(node* root){
 	inorder(root->right);
 }
 
-void leaf(node* root){
-	if(root->left==NULL && root->right==NULL){
-		printf("%d\n",root->data);
-		return;
+node* replacer(node* root){
+	if(root->left!=NULL){
+		node* temp=root->left;
+		while(temp->right!=NULL)
+			temp=temp->right;
+		return temp;
+	}else{
+		return root->right;
 	}
-	if(root->left)
-		leaf(root->left);
-	if(root->right)
-	leaf(root->right);
-		
 }
 
-node* temp;
-void mirror(node* root){
+node* search(node* root,int key){
+	node* temp=root;
+	while(1){
+		if(temp==NULL)
+			return temp;
+		else{
+			if(temp->data==key){
+				return temp;
+			}else{
+				if(temp->data > key){
+					temp=temp->left;
+				}else{
+					temp=temp->right;
+				}
+			}
+		}
+	}
+}
+
+void leaf(node* root){
 	if(root==NULL)
 		return;
-	
-	mirror(root->left);
-	mirror(root->right);
-
-	temp=root->left;
-	root->left=root->right;
-	root->right=temp;
-}
-
-int height(node* root){
-	if(root==NULL){
-		//last tak jayenga
-		//aur jab returm hote jayenga tab increment
-		return 0;
-	}else{
-		int left=height(root->left);
-		int right=height(root->right);
-		
-		if(left > right)
-			return(left+1);
-		else
-			return(right+1);
-	}
+	leaf(root->left);
+	if(root->left==NULL && root->right==NULL)
+		printf("\n%d",root->data);
+	leaf(root->right);
 	
 }
 
 node* del(node* root,int key){
-	if(root->data==key){
-		if(root->left==NULL && root->right==NULL)
-			free(root);
-		else{
-			if()
-		}
-	}else{
-		if(root->data > key)
-			root->left=del(root->left,key);
-		else
-			root->right=del(root->right,key)
+	node* iPre;
+	node* temp=root;
+    if (root == NULL){
+        return NULL;
+    }
+    if (temp->left==NULL&&temp->right==NULL){
+        free(root);
+        return NULL;
+    }
+
+    //searching for the node to be deleted
+    if (key < temp->data){
+        temp-> left = del(temp->left,key);
+    }
+    else if (key > temp->data){
+        temp-> right = del(temp->right,key);
+    }
+    //deletion strategy when the node is found
+    else{
+        iPre = replacer(temp);
+        temp->data = iPre->data;
+        temp->left = del(temp->left, iPre->data);
+    }
+    return temp;
+}
+
+int height(node* root){
+	if(root==NULL)
+		return 0;
+	else{
+		int left=height(root->left);
+		int right=height(root->right);
+		if(left > right){
+			return left+1;
+		}else
+			return right+1;
 	}
 }
 
-bool search(node *root,node* temp,int key){
-	temp=root;
-	while(temp!=NULL){
-		if(temp->)
-	}
+
+void mirror(node* root){
+	if(root==NULL)
+		return;
+		
+	mirror(root->left);
+	mirror(root->right);
+	
+	node* temp=root->left;
+	root->left=root->right;
+	root->right=temp;
 }
 
 int main(){
 	node* root=NULL;
 	root=create(root,12);
-	root=create(root,13);
 	root=create(root,11);
+	root=create(root,13);
 	root=create(root,10);
+	root=create(root,9);
+//	inorder(root);
+//	node* temp=search(root,9);
+//	if(temp!=NULL)
+//		printf("\n%d",temp->data);
+//	else
+//		printf("\nnot found");
+//		
+//	del(root,12);
 	inorder(root);
-	
-	node* tempRoot=NULL;
-	printf("\n=========\n");
 	leaf(root);
-	printf("\n=========\n");
+	int h=height(root)-1;
+	printf("\nheight:%d",h);
+
 	mirror(root);
 	inorder(root);
-	printf("\n=========\n");
-	int i=height(root);
-	printf("%d ",i-1);
 }
